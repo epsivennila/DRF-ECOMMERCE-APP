a81a735f34a257cb473777f2cb77831bc16ee518
Epsi Vennila <epsivennila@gmail.com>
2024-12-31 15:35:10 +0530

    feat:nested serializer for orderitem

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,4 +5,40 @@ from .models import Product, Order, OrderItem
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ("name", "description", "price", "stock")
+        fields = (
+            'id',
+            'name',
+            'price',
+            'stock',
+        )
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError(
+                "Price must be greater than 0."
+            )
+        return value
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source='product.name')
+    product_price = serializers.DecimalField(
+        max_digits=10, decimal_places=2, source='product.price')
+
+    class Meta:
+        model = OrderItem
+        fields = ('product_name', 'product_price', 'quantity', 'item_subtotal')
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField(method_name='total')
+
+    class Meta:
+        model = Order
+        fields = ('order_id', 'created_at', 'user',
+                  'status', 'items', "total_price")
+
+    def total(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, OrderItem, Order
+from .serializers import ProductSerializer, OrderItemSerializer, OrderSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -16,4 +16,11 @@ def product_list(request):
 def product_view(request, pk):
     product = get_object_or_404(Product, pk=pk)
     serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def order_list(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
